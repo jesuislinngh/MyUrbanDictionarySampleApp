b9af441e22cb7a4971890fb52c1ece6faa45a180
The doer <jesuislinn@gmail.com>
2019-10-29 01:02:52 -0500

    Adding a definitions model to hold the definitions gotten from the api call response

--- app/src/main/java/com/fireguard/android/myurbandictionarysampleapp/DefinitionItem.kt
+++ app/src/main/java/com/fireguard/android/myurbandictionarysampleapp/DefinitionItem.kt
@@ -1,2 +1,24 @@
 package com.fireguard.android.myurbandictionarysampleapp
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class DefinitionList(
+    val list: List<DefinitionItem>
+) : Parcelable
+
+@Parcelize
+data class DefinitionItem(
+    val definition: String,
+    val permalink: String,
+    val thumbs_up: Int,
+    val sound_urls: List<String>,
+    val author: String,
+    val word: String,
+    val defid: Int,
+    val current_vote: String,
+    val written_on: String,
+    val example: String,
+    val thumbs_down: Int
+) : Parcelable
--- app/src/main/java/com/fireguard/android/myurbandictionarysampleapp/DefinitionsViewModel.kt
+++ app/src/main/java/com/fireguard/android/myurbandictionarysampleapp/DefinitionsViewModel.kt
@@ -1,2 +1,68 @@
 package com.fireguard.android.myurbandictionarysampleapp
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+
+enum class DictionaryApiStatus { LOADING, ERROR, DONE }
+
+class DefinitionViewModel : ViewModel() {
+
+    private val TAG = DefinitionViewModel::class.java.canonicalName
+
+    // Create a Coroutine scope using a job to be able to cancel when needed
+    private var viewModelJob = Job()
+
+    // the Coroutine runs using the Main (UI) dispatcher
+    private val coroutineScope = CoroutineScope(viewModelJob + Dispatchers.Main)
+
+    // The internal MutableLiveData that stores the status of the most recent request
+    private val _status = MutableLiveData<DictionaryApiStatus>()
+
+    // The external immutable LiveData for the request status
+    val status: LiveData<DictionaryApiStatus>
+        get() = _status
+
+    // Internally, we use a MutableLiveData, because we will be updating the List of MarsProperty
+    // with new values
+    private val _properties = MutableLiveData<List<DefinitionItem>>()
+
+    // The external LiveData interface to the property is immutable, so only this class can modify
+    val definitions: LiveData<List<DefinitionItem>>
+        get() = _properties
+
+
+    fun getTermDefinitions(term: String) {
+
+        coroutineScope.launch {
+            // Get the Deferred object for our Retrofit request
+            val getTermsDeferred = DictionaryApi.retrofitService.getTermAsync(term)
+            try {
+                _status.value = DictionaryApiStatus.LOADING
+                // this will run on a thread managed by Retrofit
+                val listResult = getTermsDeferred.await()
+                _status.value = DictionaryApiStatus.DONE
+                _properties.value = listResult.list
+                Log.d(TAG, "we got a list from the server: ${listResult.list.size}")
+            } catch (e: Exception) {
+                _status.value = DictionaryApiStatus.ERROR
+                _properties.value = ArrayList()
+            }
+        }
+
+    }
+
+    /**
+     * When the [ViewModel] is finished, we cancel our coroutine [viewModelJob], which tells the
+     * Retrofit service to stop.
+     */
+    override fun onCleared() {
+        super.onCleared()
+        viewModelJob.cancel()
+    }
+}
